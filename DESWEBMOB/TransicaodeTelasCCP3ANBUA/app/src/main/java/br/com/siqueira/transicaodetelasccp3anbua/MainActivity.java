21e10d5db3f7858fbbf46e4d78f5fc4c2a8cc793
Leonardo Siqueira <lspessanha@gmail.com>
2018-08-27 21:40:52 -0300

    Inclusão do Ciclo de VIda

--- DESWEBMOB/TransicaodeTelasCCP3ANBUA/app/src/main/java/br/com/siqueira/transicaodetelasccp3anbua/MainActivity.java
+++ DESWEBMOB/TransicaodeTelasCCP3ANBUA/app/src/main/java/br/com/siqueira/transicaodetelasccp3anbua/MainActivity.java
@@ -1,8 +1,11 @@
 package br.com.siqueira.transicaodetelasccp3anbua;
 
 import android.content.Intent;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,18 +18,56 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private EditText mensagemEditText;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("ciclodevida", "MainActivity:onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("ciclodevida", "MainActivity:onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("ciclodevida", "MainActivity:onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("ciclodevida", "MainActivity:onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("ciclodevida", "MainActivity:onResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("ciclodevida", "MainActivity:onStart");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("ciclodevida", "MainActivity:onCreate");
         mensagemEditText = findViewById(R.id.mensagemEditText);
     }
 
     public void enviarMensagem (View view){
         String mensagem = mensagemEditText.getEditableText().toString();
         //Toast.makeText(this, mensagem, Toast.LENGTH_LONG).show();
-        Intent conteudo = new Intent(view.getContext(), RespostaActivity.class);
-        conteudo.putExtra("resposta",mensagem);
-        startActivity(conteudo);
+        Intent intent = new Intent(this, RespostaActivity.class);
+        intent.putExtra("resposta",mensagem);
+        startActivity(intent);
     }
 }
--- DESWEBMOB/TransicaodeTelasCCP3ANBUA/app/src/main/java/br/com/siqueira/transicaodetelasccp3anbua/RespostaActivity.java
+++ DESWEBMOB/TransicaodeTelasCCP3ANBUA/app/src/main/java/br/com/siqueira/transicaodetelasccp3anbua/RespostaActivity.java
@@ -2,6 +2,7 @@ package br.com.siqueira.transicaodetelasccp3anbua;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 /**
@@ -11,13 +12,52 @@ import android.widget.TextView;
 
 public class RespostaActivity extends AppCompatActivity {
 
-    private TextView textView;
+    private TextView textoTextView;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("ciclodevida", "RespostaActivity:onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("ciclodevida", "RespostaActivity:onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("ciclodevida", "RespostaActivity:onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("ciclodevida", "RespostaActivity:onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("ciclodevida", "RespostaActivity:onResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("ciclodevida", "RespostaActivity:onStart");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resposta);
+        Log.i("ciclodevida", "RespostaActivity:onCreate");
+        String mensagem = getIntent().getStringExtra("resposta");
 
-        textView = (TextView)findViewById(R.id.resposta);
-        textView.setText(getIntent().getStringExtra("resposta"));
+        textoTextView = (TextView)findViewById(R.id.resposta);
+        textoTextView.setText(mensagem);
     }
 }
