2dfefa9d9e9f8613f9e1873497b43fd7261ba4ed
Leonardo Siqueira <lspessanha@gmail.com>
2018-09-10 21:47:44 -0300

    Criação do List Item com o LayoutInflater

---
+++ DESWEBMOB/ServiceDeskCCP3ANBUA/app/src/main/java/br/com/siqueira/servicedeskccp3anbua/ChamadosAdapter.java
@@ -0,0 +1,49 @@
+package br.com.siqueira.servicedeskccp3anbua;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ChamadosAdapter extends ArrayAdapter <Chamado> {
+
+    private List <Chamado> chamados;
+    private Context context;
+    public ChamadosAdapter (List <Chamado> chamados, Context context){
+        super(context, -1, chamados);
+        this.chamados = chamados;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return chamados.size();
+    }
+
+    @Override
+    public Chamado getItem(int position) {
+        return chamados.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Chamado deInteresse = chamados.get(position);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View v = inflater.inflate(R.layout.list_item, parent, false);
+        TextView filaDoItemTextView = v.findViewById(R.id.filaDoItemTextView);
+        TextView descricaoDoItemTextView = v.findViewById(R.id.descricaoDoItemTextView);
+        filaDoItemTextView.setText(deInteresse.getFila());
+        descricaoDoItemTextView.setText(deInteresse.getDescricao());
+        return v;
+    }
+}
--- DESWEBMOB/ServiceDeskCCP3ANBUA/app/src/main/java/br/com/siqueira/servicedeskccp3anbua/ListaChamadosActivity.java
+++ DESWEBMOB/ServiceDeskCCP3ANBUA/app/src/main/java/br/com/siqueira/servicedeskccp3anbua/ListaChamadosActivity.java
@@ -15,72 +15,71 @@ import java.util.List;
 public class ListaChamadosActivity extends AppCompatActivity {
 
     private ListView listView;
-    private List <String> chamados;
-    private ArrayAdapter<String> adapter;
+    private List <Chamado> chamados;
+    private ChamadosAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_chamados);
         listView = (ListView) findViewById(R.id.listView);
         chamados = new ArrayList<>();
-        adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, chamados);
+        adapter = new ChamadosAdapter(chamados, this);
         listView.setAdapter(adapter);
         Intent intent = getIntent();
         String valor = intent.
                 getStringExtra(MainActivity.NOME_FILA);
-         chamados.addAll(buscaChamados(valor));
-         adapter.notifyDataSetChanged();
-         ListView.OnItemClickListener itemClickListener =
-                 new AdapterView.OnItemClickListener() {
-                     @Override
-                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        String oCaraQueFoiTocado = chamados.get(position);
-                        Toast.makeText(ListaChamadosActivity.this,
-                                 oCaraQueFoiTocado, Toast.LENGTH_SHORT).show();
-                     }
-                 };
+        chamados.addAll(buscaChamados(valor));
+        adapter.notifyDataSetChanged();
+        ListView.OnItemClickListener itemClickListener =
+             (parent, view, position, id) -> {
+                 Chamado oCaraQueFoiTocado = chamados.get(position);
+                 //Toast.makeText(ListaChamadosActivity.this,
+                 //oCaraQueFoiTocado, Toast.LENGTH_SHORT).show();
+                 Intent detalhesIntent = new Intent(this, DetalhesActivity.class);
+                 detalhesIntent.putExtra("oCaraQueFoiTocado", oCaraQueFoiTocado);
+                 startActivity(detalhesIntent);
+             };
          listView.setOnItemClickListener(itemClickListener);
     }
 
-    public ArrayList<String> buscaChamados(String chave){
-        ArrayList<String> lista = geraListaChamados();
+    public ArrayList<Chamado> buscaChamados(String chave){
+        ArrayList<Chamado> lista = geraListaChamados();
         if (chave == null || chave.length() == 0){
             return lista;
         } else {
-            ArrayList<String> subLista = new ArrayList<>();
-            for(String nome:lista){
-                if(nome.toUpperCase().contains(chave.toUpperCase())){
-                    subLista.add(nome);
+            ArrayList<Chamado> subLista = new ArrayList<>();
+            for(Chamado chamado:lista){
+                if(chamado.getDescricao().toUpperCase().contains(chave.toUpperCase())){
+                    subLista.add(chamado);
                 }
             }
             return subLista;
         }
     }
 
-    public ArrayList<String> geraListaChamados(){
-        ArrayList<String> lista = new ArrayList<>();
-        lista.add("Desktops: Computador da secretária quebrado.");
-        lista.add("Telefonia: Telefone não funciona.");
-        lista.add("Redes: Manutenção no proxy.");
-        lista.add("Servidores: Lentidão generalizada.");
-        lista.add("Novos Projetos: CRM");
-        lista.add("Manutenção Sistema ERP: atualizar versão.");
-        lista.add("Novos Projetos: Rede MPLS");
-        lista.add("Manutenção Sistema de Vendas: incluir pipeline.");
-        lista.add("Manutenção Sistema ERP: erro contábil");
-        lista.add("Novos Projetos: Gestão de Orçamento");
-        lista.add("Novos Projetos: Big Data");
-        lista.add("Manoel de Barros");
-        lista.add("Redes: Internet com lentidão");
-        lista.add("Novos Projetos: Chatbot");
-        lista.add("Desktops: troca de senha");
-        lista.add("Desktops: falha no Windows");
-        lista.add("Novos Projetos: ITIL V3");
-        lista.add("Telefonia: liberar celular");
-        lista.add("Telefonia: mover ramal");
-        lista.add("Redes: ponto com defeito");
-        lista.add("Novos Projetos: ferramenta EMM");
+    public ArrayList<Chamado> geraListaChamados(){
+        ArrayList<Chamado> lista = new ArrayList<>();
+        lista.add(new Chamado("Desktops", "Computador da secretária quebrado."));
+        lista.add(new Chamado("Telefonia", "Telefone não funciona."));
+        lista.add(new Chamado("Redes", "Manutenção no proxy."));
+        lista.add(new Chamado("Servidores", "Lentidão generalizada."));
+        lista.add(new Chamado("Novos Projetos", "CRM"));
+        lista.add(new Chamado("Manutenção Sistema ERP", "atualizar versão."));
+        lista.add(new Chamado("Novos Projetos", "Rede MPLS"));
+        lista.add(new Chamado("Manutenção Sistema de Vendas", "incluir pipeline."));
+        lista.add(new Chamado("Manutenção Sistema ERP", "erro contábil"));
+        lista.add(new Chamado("Novos Projetos", "Gestão de Orçamento"));
+        lista.add(new Chamado("Novos Projetos", "Big Data"));
+        lista.add(new Chamado("Nome", "Manoel de Barros"));
+        lista.add(new Chamado("Redes", "Internet com lentidão"));
+        lista.add(new Chamado("Novos Projetos", "Chatbot"));
+        lista.add(new Chamado("Desktops", "troca de senha"));
+        lista.add(new Chamado("Desktops", "falha no Windows"));
+        lista.add(new Chamado("Novos Projetos", "ITIL V3"));
+        lista.add(new Chamado("Telefonia", "liberar celular"));
+        lista.add(new Chamado("Telefonia", "mover ramal"));
+        lista.add(new Chamado("Redes", "ponto com defeito"));
+        lista.add(new Chamado("Novos Projetos", "ferramenta EMM"));
         return lista;
     }
 }
